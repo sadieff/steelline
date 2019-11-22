78cdc26a4aa24840f7823c1d5e43624f0fd8de8c
sadieff <savintsev.dima@gmail.com>
2019-11-22 11:08:26 +0300

    Верстка, первая версия

---
+++ src/js/main.js
@@ -0,0 +1,70 @@
+$(document).ready(function() {
+
+    /* menu */
+
+    $('.menu-open').on('click', function(){
+        $(this).toggleClass('active');
+        $('.header-menu').toggleClass('active');
+        $('body').toggleClass('menu-opened');
+    });
+
+    $('.header-menu li.parent > span').on('click', function(){
+        var content = $(this).next('ul');
+        if(content.is(':hidden')){
+            content.slideDown(300);
+        }
+        else {
+            content.slideUp(300);
+        }
+    });
+
+    /* filter */
+
+    $('.category-open_filter').on('click', function(){
+        $.fancybox.open({
+            src  : '.category-filter',
+            type : 'inline',
+            attr: {
+                scrolling: "none"
+            },
+            scrolling : 'visible'
+        });
+    });
+
+    /* slider */
+
+    if (document.getElementById('slider')) {
+        var slider = document.getElementById('slider');
+        noUiSlider.create(slider, {
+            start: [61000, 180000],
+            connect: true,
+            range: {
+                'min': 0,
+                'max': 250000
+            }
+        });
+        var minprice = document.getElementById('minprice');
+        var maxprice = document.getElementById('maxprice');
+
+        minprice.addEventListener('change', function () {
+            slider.noUiSlider.set([this.value, null]);
+        });
+        maxprice.addEventListener('change', function () {
+            slider.noUiSlider.set([null, this.value]);
+        });
+
+        slider.noUiSlider.on('update', function (values, handle) {
+            var value = values[handle];
+            value = value.split('.');
+            value = value[0];
+            if (handle) {
+                maxprice.value = value;
+                $('.maxprice').text(value);
+            } else {
+                minprice.value = value;
+                $('.minprice').text(value);
+            }
+        });
+    }
+
+});
